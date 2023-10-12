d5dd5b3f59fe4205b6da26a15622ff68cd3147f2
John Doe <johndoe@email.com>
2023-10-12 16:22:32 +0530

    brought questions from db

--- public/scripts/homepage.js
+++ public/scripts/homepage.js
@@ -1,9 +1,37 @@
 console.log("attached");
 const add_question_btn = document.querySelector(".add_question_btn");
 const question_input = document.querySelector(".question_input");
+const output_div = document.querySelector(".output_div");
+// console.log(add_question_btn);
 
-console.log(add_question_btn);
+function addtopage(data){
+   // console.log('js wala data',data);
+    const dataarray =data.data;
+    //console.log('data.data',dataarray);
+    output_div.innerHTML = '';
+    // const output_div= document.createElement("div");
+    
+    const questions_display = document.createElement("div");
+    questions_display.classList.add("questions_display");
 
+    const dataquestion = document.createElement("div");
+    //dataquestion.dataset.dataquestion = data-question;
+
+    question_div.dataset.uniqueid=questions[i].Q_id;
+
+
+    questions_display.appendChild( dataquestion);
+
+    output_div.appendChild( questions_display);
+    
+
+
+for(let i=0; i<dataarray.length; i++){
+  
+   console.log(dataarray[i].question);
+}
+
+}
 
 add_question_btn.addEventListener("click", async (e) => {
     
@@ -17,7 +45,8 @@ add_question_btn.addEventListener("click", async (e) => {
         })
         
         question_input.value = '';
-        console.log('js wala data',data);
+       // console.log('js wala data',data);
+        addtopage(data);
         
         }
         catch (e) {}
